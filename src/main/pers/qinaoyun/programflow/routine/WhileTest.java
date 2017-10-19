2ad82384b77ae2d7c1b3d08b672e7882e0406291
qinaoyun <2509376331@qq.com>
2017-10-19 17:28:03 +0800

    Java编程思想 第 3 章 例程 白盒测试

---
+++ src/main/pers/qinaoyun/programflow/routine/WhileTest.java
@@ -0,0 +1,15 @@
+package main.pers.qinaoyun.programflow.routine;
+
+/**
+ * @author qinaoyun
+ * 白盒测试
+ */
+public class WhileTest {
+    public static void main(String[] args) {
+        double r = 0;
+        while (r < 0.99d) {
+            r = Math.random();
+            System.out.println(r);
+        }
+    }
+}
