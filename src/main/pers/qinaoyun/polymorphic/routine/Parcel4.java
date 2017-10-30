3e292d7b5604f3768609aba59c0e0eed927a3b52
qinaoyun <2509376331@qq.com>
2017-10-30 16:34:35 +0800

    Java编程思想 第 7 章 多形性 测试内部类的作用域

---
+++ src/main/pers/qinaoyun/polymorphic/routine/Parcel4.java
@@ -0,0 +1,31 @@
+package main.pers.qinaoyun.polymorphic.routine;
+
+/**
+ * Description:测试内部类的作用域
+ *
+ * @author qinaoyun
+ *         Date: 2017-10-30
+ *         Time: 16:17
+ */
+public class Parcel4 {
+    public Destination dest(String s) {
+        class PDestination
+                implements Destination {
+            private String label;
+
+            private PDestination(String whereTo) {
+                label = whereTo;
+            }
+
+            public String readLabel() {
+                return label;
+            }
+        }
+        return new PDestination(s);
+    }
+
+    public static void main(String[] args) {
+        Parcel4 p = new Parcel4();
+        Destination d = p.dest("Tanzania");
+    }
+} ///:~
